4382f411e3e897dd34ec07a63bd65f77212a57ce
kenjones1973 <45968636+kenjones1973@users.noreply.github.com>
2019-04-09 14:16:01 +0100

    Update Refresh_Container_JS.bat

--- Refresh_Container_JS.bat
+++ Refresh_Container_JS.bat
@@ -1,1 +1,1 @@
-C:\devops\dxtoolkit2\dx_ctl_js_container -d Delphix5302 -action refresh -container_name ORCL_CONTAINER
+C:\devops\dxtoolkit2\dx_ctl_js_container -d Delphix5302 -action refresh -container_name ORCL_CONTAINER1
